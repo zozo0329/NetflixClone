c3402c780c4e0a319200cf08dc6408e0f2cbe3e1
Zo <161946731+zozo0329@users.noreply.github.com>
2024-10-17 22:54:07 +0800

    minor update

---
+++ sign-in.js
@@ -0,0 +1,22 @@
+document.querySelector("#signin-btn").addEventListener("click", function () {
+  let audio = new Audio("music/pou-sound-effect.mp3");
+  audio.play();
+});
+document.querySelector("#use-a-code").addEventListener("click", function () {
+  let audio = new Audio("music/pou-sound-effect.mp3");
+  audio.play();
+});
+document
+  .querySelector(".input-section a")
+  .addEventListener("click", function () {
+    let audio = new Audio("music/pou-sound-effect.mp3");
+    audio.play();
+  });
+document.querySelector(".reminder a").addEventListener("click", function () {
+  let audio = new Audio("music/pou-sound-effect.mp3");
+  audio.play();
+});
+document.querySelector(".notes a").addEventListener("click", function () {
+  let audio = new Audio("music/pou-sound-effect.mp3");
+  audio.play();
+});
