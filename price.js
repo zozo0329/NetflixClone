ebac18ed91d3b43ecf97d3156c26813d76480781
Zo <161946731+zozo0329@users.noreply.github.com>
2024-10-21 04:09:04 +0800

    added pricing plan page

---
+++ price.js
@@ -0,0 +1,6 @@
+document.querySelector(".dropdown").addEventListener("click", function () {
+  document.querySelector(".options").style.display = "block";
+});
+document.querySelector("ul").addEventListener("click", function () {
+  document.querySelector(".options").style.display = "none";
+});
