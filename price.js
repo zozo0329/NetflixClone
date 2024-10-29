c41eb6a6b035e39affd7aed026ce9b20f1ce6e2f
Zo <161946731+zozo0329@users.noreply.github.com>
2024-10-29 10:17:08 +0800

    dropdown is now change

--- price.js
+++ price.js
@@ -1,7 +1,37 @@
-document.querySelector(".dropdown").addEventListener("click", function () {
-  document.querySelector(".options").style.display = "block";
-});
+// document.querySelector(".dropdown").addEventListener("click", function () {
+//   document.querySelector(".options").style.display = "block";
+// });
 
-document.querySelector(".options").addEventListener("click", function () {
-  document.querySelector(".options").style.display = "none";
-});
+// document.querySelector(".options").addEventListener("click", function () {
+//   document.querySelector(".options").style.display = "none";
+// });
+// let a = document.getElementById("cars");
+// let value = a.value;
+
+// for (var i = 0; i < value; i++) {
+//   // document.querySelectorAll("option")[i].addEventListener("click", function () {
+//   //   console.log("qwe");
+//   // });
+//   console.log("asd");
+// }
+//
+// function abc() {
+//   const x = document.getElementById("cars").value;
+//   console.log(x);
+// }
+
+// const x = document.querySelector("#cars");
+// document.getElementById("cars").addEventListener("change", function (a) {
+//   console.log(a);
+// });
+// const Cat = {
+//   name: "Whiskers",
+//   age: 3,
+//   breed: "Siamese",
+//   color: "Cream",
+// };
+// console.log(Cat);
+// document.querySelector("h1").innerText = "Name:" + Cat.name;
+// document.querySelector("h2").innerText = "Age:" + Cat.age;
+// document.querySelector("h3").innerText = "Breed:" + Cat.breed;
+// document.querySelector("h4").innerText = "Color:" + Cat.color;
