efdf5476cfb7d72770a5e22e750957cc45f37b9d
Zo <161946731+zozo0329@users.noreply.github.com>
2024-10-30 15:14:04 +0800

    the dropdown is now fixed

--- price.js
+++ price.js
@@ -35,3 +35,46 @@
 // document.querySelector("h2").innerText = "Age:" + Cat.age;
 // document.querySelector("h3").innerText = "Breed:" + Cat.breed;
 // document.querySelector("h4").innerText = "Color:" + Cat.color;
+// country();
+// async function country() {
+//   try {
+//     const fetchCountry = await fetch(`https://restcountries.eu/rest/v2/all`);
+//     if (!fetchCountry.ok) {
+//       return console.log("Sorry, not working");
+//     }
+//     console.log(fetchCountry);
+//     const countryObject = await fetchCountry.json();
+//     const countryContainer = countryObject.name;
+//     //
+//     // countryObject.map(function (value) {
+//     //   const countryName = document.createElement("option");
+//     //   countryName.innerText = value.name;
+//     //   document.querySelector("select").appendChild(countryName);
+//     // });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+async function country() {
+  try {
+    const res = await fetch(`https://restcountries.com/v3.1/all`);
+    if (!res.ok) {
+      return console.log("Sorry, not working");
+    }
+    const obj = await res.json();
+    const sortedNames = obj.sort((a, b) =>
+      a.name.common.localeCompare(b.name.common)
+    );
+    const selectId = document.querySelector("#country");
+    obj.forEach((country) => {
+      const option = document.createElement("option");
+      //  console.log((option.value = country.name));
+      option.value = country.name;
+      option.innerText = country.name.common;
+      selectId.appendChild(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+country();
